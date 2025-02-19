d54f8facf6b4f7904744cc1a6e11149f7f600b7a
tasso905 <tasso75@hotmail.com>
2025-02-19 13:42:28 -0500

    fixed 403 and image load error

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ import wikipedia
 
 Builder.load_file('frontend.kv')
 
+headers = {
+    'User-Agent': 'My User Agent 1.0'
+}
+
 class FirstScreen(Screen):
     def search_image(self):
         #get user query from the text box (TextInput)
         query = self.manager.current_screen.ids.user_query.text
         #get wikipedia page and the first image from list
-        page = wikipedia.page(query)
+        page = wikipedia.page(query, auto_suggest=False, redirect=True, preload=False)
         image_link = page.images[0]
         #download the image using requests
-        req = requests.get(image_link)
+        req = requests.get(image_link, headers=headers)
         #write image to file
         imagepath = 'files/image.jpg'
         with open(imagepath, 'wb') as file:
-            file.write(imagepath)
+            file.write(req.content)
         #set image in the kivy Image widget
         self.manager.current_screen.ids.img.source = imagepath
 
